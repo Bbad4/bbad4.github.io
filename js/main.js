63c0ca8e68a34ca7b0af8c4af028915a32f39871
Bbad4 <pratikpwadkar@gmail.com>
2025-06-27 07:23:25 +0530

    add firebase

---
+++ js/main.js
@@ -0,0 +1,53 @@
+import { app, analytics, auth, messaging, messagingSw } from './firebase.js'
+import { appObj, packageName, vapidKey, logo } from './firebase.js'
+import { sendSignInLinkToEmail, signInWithEmailLink, getToken, parsePayload } from './functions.js'
+
+
+const analyticsObj = analytics.getAnalytics(appObj)
+const authObj = auth.getAuth(appObj)
+const messagingObj = messaging.getMessaging(appObj)
+
+
+//vibrate
+document.addEventListener('click', (event) => event.target.closest('button') && navigator.vibrate(10))
+
+
+//sendSignInLinkToEmail
+document.getElementById('email_login').addEventListener('click', async () => {
+    await sendSignInLinkToEmail(authObj, packageName)
+})
+
+
+//signInWithEmailLink
+if (await auth.isSignInWithEmailLink(authObj, location.href)) {
+    await signInWithEmailLink(authObj)
+}
+
+
+//getToken
+if (await Notification.requestPermission() === 'granted') {
+    await getToken(messagingObj, vapidKey)
+}
+
+
+//onMessage
+messaging.onMessage(messagingObj, (payload) => {
+    const { title, options } = parsePayload(payload, logo)
+    new Notification(title, options)
+})
+
+
+
+
+
+
+//onAuthStateChanged
+async function f0(usercreds) {
+  if (usercreds) {
+    document.querySelector('.button_fab').style.backgroundColor = "green";
+  } else {
+    document.querySelector('.button_fab').style.backgroundColor = "red";
+  }
+}
+
+auth.onAuthStateChanged(authObj, f0);
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-// service-worker.js
+import { app, analytics, auth, messaging, messagingSw } from './js/firebase.js'
+import { appObj, logo } from './js/firebase.js'
+import { parsePayload } from './js/functions.js'
+
+
+const messagingSwObj = messagingSw.getMessaging(appObj)
+
+
+//onBackgroundMessage
+messagingSw.onBackgroundMessage(messagingSwObj, (payload) => {
+    const { title, options } = parsePayload(payload, logo)
+    self.registration.showNotification(title, options)
+})
