128d23e1c265fa7c9ff2f03224f670d0422198c6
Bbad4 <pratikpwadkar@gmail.com>
2025-08-09 21:29:10 +0530

    update vibration and micro interaction

--- js/main.js
+++ js/main.js
@@ -14,4 +14,8 @@ analytics.getAnalytics(appObj)
 
 
 //
-document.addEventListener('click', (event) => event.target.closest('button') && navigator.vibrate?.(15))
+document.addEventListener('click', (event) => {
+    if (event.target.closest('body > * > *') && !event.target.closest('.grid_intro > :nth-child(2)')) {
+        navigator.vibrate?.(10)
+    }
+})
